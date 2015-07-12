328045f1d8c64d6f948202022e863665132236f0
Stephanie Koltun <stephkoltun@icloud.com>
2015-07-11 22:16:23 -0400

    sort of working, but not convinced

--- js/script.js
+++ js/script.js
@@ -20,46 +20,38 @@ function generateColorBar(dir, id) {
                 // add wrapper and image
                 $("#" + id).prepend("<div class='imageWrapper' id='moment" + counter + "'><img class='barImage' id='image" + counter + "'" +"src=" + dir + filename + "></img>");
             });
-            addColor(id);
         }
 
-    }); // end ajax request
+    }).done(function() {
 
-}
-
-
-
-
-
-function addColor(id) {
     console.log("ajax complete! color processing begin!");
 
-    var images = $("#" + id + " img");
-    var wrappers = $("#" + id + " div");
+            var images = $("#" + id + " img");
+            var wrappers = $("#" + id + " div");
 
-    for (i = 0; i < images.length; i++) {
+            for (i = 0; i < images.length; i++) {
 
-        var currentImageID = images[i].id;
-        var currentImage = document.getElementById(currentImageID);
+                var currentImageID = images[i].id;
+                var currentImage = document.getElementById(currentImageID);
 
-        var currentWrapperID = wrappers[i].id;
-        var currentWrapper = document.getElementById(currentWrapperID);
+                var currentWrapperID = wrappers[i].id;
+                var currentWrapper = document.getElementById(currentWrapperID);
 
-        var vibrant = new Vibrant(currentImage);
-        var swatches = vibrant.swatches();
-        var dominantColor = swatches.Vibrant.getHex();
-
-
+                var vibrant = new Vibrant(currentImage);
+                var swatches = vibrant.swatches();
+                var dominantColor = swatches.Vibrant.getHex();
 
 
-        // add div with dominant color
-        $(currentWrapper).prepend("<div class='box' id='box" + (i+1) + "'></div>");
 
-        $("#box" + (i+1)).css("background-color", dominantColor);
-    }
+
+                // add div with dominant color
+                $(currentWrapper).append("<div class='box' id='box" + (i+1) + "'></div>");
+
+                $("#box" + (i+1)).css("background-color", dominantColor);
+            }
+
+});
 }
-
-
 
 
 
